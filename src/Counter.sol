5c4272ddaded2b69b7a0f1cf5ef5fbac9d1753cf
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-01-28 23:09:51 +0530

    feat: ✨ use `ERC20` in `Counter`

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Counter is ERC20 {
     uint256 public number;
+
+    constructor(string memory name_, string memory symbol_)
+        ERC20(name_, symbol_)
+    {}
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
