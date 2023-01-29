a5fb56d216669af511e26bcff59bc4f226f586de
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-01-29 21:47:30 +0530

    feat: ✨ add `StakeContract`

---
+++ src/StakeContract.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+error TransferFailed();
+
+contract StakeContract {
+    // user => token => amount
+    mapping(address => mapping(address => uint256)) balances;
+
+    function stake(address token, uint256 amount) external {
+        balances[msg.sender][token] = amount;
+        bool success = IERC20(token).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+        if (!success) revert TransferFailed();
+    }
+}
