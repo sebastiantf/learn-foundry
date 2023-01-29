fa3d21e5c574ffb6cc86400b7f4148324b8d5b86
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-01-29 21:55:02 +0530

    test: 🧪 add `StakeContractTest`

---
+++ test/StakeContract.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/StakeContract.sol";
+import "./mocks/MockERC20.sol";
+
+contract StakeContractTest is Test {
+    StakeContract public stakeContract;
+    MockERC20 public mockERC20;
+
+    function setUp() public {
+        stakeContract = new StakeContract();
+        mockERC20 = new MockERC20("MockERC20", "MERC20");
+    }
+
+    function testStake() public {
+        uint256 amount = 10e18;
+        mockERC20.approve(address(stakeContract), amount);
+
+        bool success = stakeContract.stake(address(mockERC20), amount);
+        assertTrue(success);
+    }
+}
