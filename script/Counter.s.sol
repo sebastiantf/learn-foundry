8081c786fe71dd3521cca6af21326518e4f1f711
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-02-06 23:06:16 +0530

    feat: 🔨 add deployment script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
-    function run() public {
-        vm.broadcast();
+    function run() public returns (address) {
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.rememberKey(privateKey);
+
+        vm.broadcast(deployer); // comment to simulate
+        Counter c = new Counter("name", "symbol");
+        return address(c);
     }
 }
