4ab9f26baa184d1b4406b23ae83975d5ea44b3cc
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-05-01 18:51:34 +0530

    test: 🧪 disable invalid invariant_totalSupplyStaysZero

--- test/WETH9.invariants.t.sol
+++ test/WETH9.invariants.t.sol
@@ -17,12 +17,12 @@ contract WETH9Invariants is Test, InvariantTest {
         targetContract(address(handler));
     }
 
-    function invariant_totalSupplyStaysZero() public {
+    /* function invariant_totalSupplyStaysZero() public {
         // totalSupply will stay at zero since fuzzer does not fuzz msg.value,
         // which is required to deposit() and increase WETH supply
         // We need a handler method to set up those conditions
         assertEq(weth.totalSupply(), 0);
-    }
+    } */
 
     // unit test that shows its possible to deposit and mint zero amount WETH
     function test_zeroDeposit() public {
