83747f4c57acc68373697cbd65b8553ca3705b87
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-05-01 18:49:17 +0530

    test: 🧪 bound to avoid reverts

--- test/handlers/Handler.sol
+++ test/handlers/Handler.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../../src/WETH9.sol";
 
-contract Handler {
+contract Handler is Test {
     WETH9 public weth;
 
     constructor(WETH9 _weth) {
@@ -12,6 +12,9 @@ contract Handler {
     }
 
     function deposit(uint256 _amount) public {
+        // bound to available balance to avoid reverts
+        _amount = bound(_amount, 0, address(this).balance);
+
         weth.deposit{value: _amount}();
     }
 }
